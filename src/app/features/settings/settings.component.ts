d38bf50f5da4dd52d0ee41df2b45c35e3d1a0218
Mauro Molina <molinamau37@gmail.com>
2024-11-23 14:07:55 -0500

    adicionando settings

--- src/app/features/settings/settings.component.ts
+++ src/app/features/settings/settings.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faArrowRight, faEnvelope, faExclamationTriangle, faEye, faEyeSlash, faHome, faLock, faPhone, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
+  styleUrl: './settings.component.scss',
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit {
+  constructor() {}
 
+  // Icons
+  faArrow = faArrowRight;
+  faEmail = faEnvelope;
+  faLock = faLock;
+  faEye = faEye;
+  faEyeSlash = faEyeSlash;
+  faWarning = faExclamationTriangle;
+  faHome = faHome
+  faPhone = faPhone
+  faUser = faUser
+
+  ngOnInit(): void {}
+
+  getProfile() {}
 }
